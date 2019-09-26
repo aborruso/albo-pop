d67c897357f9b4359f04dc23cd4feb1c6ab2cf89
Cristiano Longo <cristianolongo@hackspacecatania.it>
2019-09-26 23:03:24 +0200

    moved catania

--- code/catania/testcut.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$date=new DateTimeImmutable();
-$pd=$date->sub(new DateInterval('P2M'));
-$nd=$date->add(new DateInterval('P2Y'));
-echo $pd->format('Y-m-d')."\n";
-echo $nd->format('Y-m-d')."\n";
-?>
